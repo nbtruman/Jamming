84674f08739497103d7a99dc7a6b40266ebe24e6
Noah Truman <104936442+nbtruman@users.noreply.github.com>
2022-10-12 18:09:40 +0100

    create Track

--- jamming/src/Components/TrackList/TrackList.js
+++ jamming/src/Components/TrackList/TrackList.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Track from "../Track/Track";
+// import Track from "../Track/Track";
 import "./TrackList.css";
 
 class TrackList extends React.Component {
     render() {
         return(
             <div className="TrackList">
-                <Track />;
+                {/* <Track />; */}
             </div>
         );
     };
