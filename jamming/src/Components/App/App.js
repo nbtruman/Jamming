bc6209a3b377e47fe168edd5ae981a6fe4b4732c
Noah Truman <104936442+nbtruman@users.noreply.github.com>
2022-10-12 13:58:34 +0100

    import SearchBar and SearchResults to App

--- jamming/src/Components/App/App.js
+++ jamming/src/Components/App/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import './App.css';
 
+import SearchBar from "../SearchBar/SearchBar";
+import SearchResults from "../SearchResults/SearchResults";
+
 class App extends React.Component {
   render() {
     return (
     <div>
       <h1>Ja<span className="highlight">mmm</span>ing</h1>
       <div className="App">
-        {/* <SearchBar /> */}
+        <SearchBar />
         <div className="App-playlist">
-          {/* <SearchResults /> */}
+          <SearchResults />
           {/* <Playlist /> */}
         </div>
       </div>
