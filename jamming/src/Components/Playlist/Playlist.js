c1de1d935e87bd3007fee332cc35d85adefa7b59
Noah Truman <104936442+nbtruman@users.noreply.github.com>
2022-10-13 16:30:36 +0100

    step 33

--- jamming/src/Components/Playlist/Playlist.js
+++ jamming/src/Components/Playlist/Playlist.js
@@ -7,7 +7,7 @@ class Playlist extends React.Component {
     render() {
         return(
             <div className="Playlist">
-                <input defaultvalue={"New Playlist"}/>
+                <input defaultValue={"New Playlist"}/>
                 <TrackList />
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
