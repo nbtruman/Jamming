15f6c553c3e8fc81ae57743f47d612435e03ac9b
Noah Truman <104936442+nbtruman@users.noreply.github.com>
2022-10-18 17:03:07 +0100

    step 73

--- jamming/src/Components/App/App.js
+++ jamming/src/Components/App/App.js
@@ -22,6 +22,11 @@ class App extends React.Component {
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
     this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
+  }
+
+  search(searchTerm) {
+    console.log(searchTerm);
   }
 
   savePlaylist() {
@@ -53,9 +58,10 @@ class App extends React.Component {
     <div>
       <h1>Ja<span className="highlight">mmm</span>ing</h1>
       <div className="App">
-        <SearchBar />
+        <SearchBar onSearch={this.search}/>
         <div className="App-playlist">
-          <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
+          <SearchResults  searchResults={this.state.searchResults} 
+                          onAdd={this.addTrack}/>
           <Playlist playlistName={this.state.playlistName} 
                     playlistTracks={this.state.playlistTracks}
                     onRemove={this.removeTrack}
